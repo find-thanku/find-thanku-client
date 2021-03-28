9ed532d646676425f686c65699d44ff6f42951eb
Jisu Lim <limjisuu@korea.ac.kr>
2021-03-29 01:48:58 +0900

    feat : add found board page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Layout from "../src/component/common/layout";
 import MainPage from "../src/view/main/index";
 import LostBoard from "../src/view/lost-board/index";
+import FoundBoard from "../src/view/found-board/index";
 
 const App: React.FC = () => (
   <div>
@@ -11,6 +12,7 @@ const App: React.FC = () => (
         <Switch>
           <Route path="/" exact component={MainPage} />
           <Route path="/lost-board" exact component={LostBoard} />
+          <Route path="/found-board" exact component={FoundBoard} />
           <Redirect from="*" to="/" />
         </Switch>
       </Layout>
--- src/component/common/header/index.tsx
+++ src/component/common/header/index.tsx
@@ -11,6 +11,9 @@ const Header = () => {
         <S.ButtonContainer to="/lost-board">
           <S.Button>분실물</S.Button>
         </S.ButtonContainer>
+        <S.ButtonContainer to="/found-board">
+          <S.Button>습득물</S.Button>
+        </S.ButtonContainer>
       </S.DesktopButtons>
       <S.MobileButtons></S.MobileButtons>
     </S.Header>
