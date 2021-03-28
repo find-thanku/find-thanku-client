db37d4447bcb88ad8edb25a403f84b9aa79cb557
Jisu Lim <limjisuu@korea.ac.kr>
2021-03-28 23:08:51 +0900

    style : change header width and height

--- src/component/common/header/styles.ts
+++ src/component/common/header/styles.ts
@@ -5,11 +5,12 @@ export const Header = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  height: 10rem;
+  width: 100%;
+  height: 5rem;
   padding: 0rem 2rem;
   border-bottom: 3px solid ${THEME_COLOR.VIOLET};
   @media only screen and (min-width: ${BREAKPOINT}px) {
-    height: 10rem;
+    height: 4rem;
     padding: 0rem 5rem;
   }
 `;
