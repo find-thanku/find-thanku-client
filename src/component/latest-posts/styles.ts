80631e3195a92db36ad555abd2c173b726e5739a
Jisu Lim <limjisuu@korea.ac.kr>
2021-05-22 10:20:57 +0900

    refactor : delete unnecessary codes

--- src/component/latest-posts/styles.ts
+++ src/component/latest-posts/styles.ts
@@ -20,8 +20,6 @@ export const TitleContainer = styled.div`
 `;
 
 export const Title = styled.div`
-  /* display: flex;
-  align-items: center; */
   padding-bottom: 0.3rem;
   font-size: 1.2rem;
   font-weight: bold;
