4d5756cc49a7156812adcc4c6d94ed27a030b081
Jisu Lim <limjisuu@korea.ac.kr>
2021-08-13 17:30:01 +0900

    feat : add write button

--- src/container/found-board-container/index.tsx
+++ src/container/found-board-container/index.tsx
@@ -3,6 +3,7 @@ import * as S from "./styles";
 import FoundPostPreview from "../../component/found-post-preview";
 import foundBoardAPI from "../../common/lib/api/found-board";
 import { FoundPostModel } from "../../common/model/found-post";
+import { STATIC_URL } from "../../asset/constant";
 
 const FoundBoardContainer = () => {
   const [page, setPage] = useState(1);
@@ -20,20 +21,27 @@ const FoundBoardContainer = () => {
 
   return (
     <S.FoundBoardContainer>
-      <S.Category>습득물</S.Category>
-      {data &&
-        data.map((post, idx) => {
-          const { title, item_name, get_place, reply_num } = post;
-          return (
-            <FoundPostPreview
-              key={idx}
-              title={title}
-              item={item_name}
-              location={get_place}
-              replyCount={reply_num}
-            />
-          );
-        })}
+      <S.CategoryContainer>
+        <S.Category>습득물</S.Category>
+      </S.CategoryContainer>
+      <S.PostContainer>
+        {data &&
+          data.map((post, idx) => {
+            const { title, item_name, get_place, reply_num } = post;
+            return (
+              <FoundPostPreview
+                key={idx}
+                title={title}
+                item={item_name}
+                location={get_place}
+                replyCount={reply_num}
+              />
+            );
+          })}
+        <S.WriteButton>
+          <S.PlusIcon src={STATIC_URL.WHITE_PLUS_ICON} />
+        </S.WriteButton>
+      </S.PostContainer>
     </S.FoundBoardContainer>
   );
 };
--- src/container/found-board-container/styles.ts
+++ src/container/found-board-container/styles.ts
@@ -5,18 +5,52 @@ export const FoundBoardContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 100vw;
+  height: 100%;
+`;
+
+export const CategoryContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   width: 100%;
-  height: 100vh;
+  height: 3rem;
+  padding: 0rem 2rem;
 `;
 
 export const Category = styled.div`
   display: flex;
   justify-content: center;
-  width: 100%;
-  height: 3rem;
   align-items: center;
   color: ${THEME_COLOR.DARK_VIOLET};
   font-size: 1.2rem;
   font-weight: bold;
-  border-bottom: 2px solid lightgray;
+`;
+
+export const PostContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  padding: 0.4rem;
+  align-items: center;
+  background-color: ${THEME_COLOR.VIOLET};
+`;
+
+export const WriteButton = styled.div`
+  z-index: 100;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  width: 3.7rem;
+  height: 3.7rem;
+  right: 0.7rem;
+  bottom: 0.7rem;
+  background-color: #353a5d;
+  border-radius: 50%;
+`;
+
+export const PlusIcon = styled.img`
+  width: 2rem;
+  height: 2rem;
 `;
