5218e203a5765cb60685d3abf09a22ae8cb7b7fb
Jisu Lim <limjisuu@korea.ac.kr>
2021-09-01 23:41:12 +0900

    feat : add lost board api

--- src/common/endpoints.ts
+++ src/common/endpoints.ts
@@ -1,6 +1,7 @@
 const endpoints = {
   API_BASE_URL: "http://127.0.0.1:8000/api",
   FOUND_BOARD_API: "/found/board",
+  LOST_BOARD_API: "/lost/board",
 };
 
 export default endpoints;
---
+++ src/common/model/lost-post.ts
@@ -0,0 +1,12 @@
+export interface LostPostModel {
+  id: number;
+  title: string;
+  item_name: string;
+  lost_place: string;
+  content: string;
+  reply_num: number;
+  created_at: string;
+  changed_at: string;
+  user_id: number;
+  image_url: string;
+}
