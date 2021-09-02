0166cc4bffb4e6f1f71de769732c31fde113cf78
Jisu Lim <limjisuu@korea.ac.kr>
2021-09-03 03:11:49 +0900

    feat : add function for increasing item and visitor count

--- src/container/main-container/index.tsx
+++ src/container/main-container/index.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import * as S from "./styles";
 import LatestPosts from "../../component/latest-posts";
 import CountInfo from "../../component/count-info";
 
 const MainContainer = () => {
+  const [lostCount, setLostCount] = useState(0);
+  const [foundCount, setFoundCount] = useState(0);
+  const [visitorCount, setVisitorCount] = useState(0);
+  const lost = 23;
+  const found = 32;
+  const visitor = 322;
+
+  const increaseLostCount = () => {
+    setTimeout(() => {
+      setLostCount(lostCount + 3);
+    }, 5);
+  };
+
+  const increaseFoundCount = () => {
+    setTimeout(() => {
+      setFoundCount(foundCount + 3);
+    }, 5);
+  };
+
+  const increaseVisitorCount = () => {
+    setTimeout(() => {
+      setVisitorCount(visitorCount + 10);
+    }, 5);
+  };
+
+  useEffect(() => {
+    if (lostCount < lost) increaseLostCount();
+  }, [lostCount]);
+
+  useEffect(() => {
+    if (foundCount < found) increaseFoundCount();
+  }, [foundCount]);
+
+  useEffect(() => {
+    if (visitorCount < visitor) increaseVisitorCount();
+  }, [visitorCount]);
+
   return (
     <S.MainContainer>
       <S.Intro>
@@ -13,9 +50,9 @@ const MainContainer = () => {
           구성원을 위한 분실물 센터입니다.
         </S.ProjectIntro>
         <S.CountInfoContainer>
-          <CountInfo title={"분실물"} count={36}></CountInfo>
-          <CountInfo title={"습득물"} count={42}></CountInfo>
-          <CountInfo title={"방문자"} count={322}></CountInfo>
+          <CountInfo title={"분실물"} count={lostCount}></CountInfo>
+          <CountInfo title={"습득물"} count={foundCount}></CountInfo>
+          <CountInfo title={"방문자"} count={visitorCount}></CountInfo>
         </S.CountInfoContainer>
       </S.Intro>
       <LatestPosts title={"공지사항"} />
